ad7e6b93852acfccbd7301bd524a272e31e284e4
madisehastu <66207049+madisehastu@users.noreply.github.com>
2021-06-27 21:33:08 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,53 +1,28 @@
 import React, { useState } from 'react'
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const Statistics = (props) => {
-    if (props.clicks === 0) {
-      return (
-        <div>
-          <h1> statistics </h1>
-          <div> No feedback given </div>
-        </div>
-      )
-    }
-    return (
-      <div>
-        <h1> statistics </h1>
-        <div> good {props.good} </div>
-        <div> neutral {props.neutral} </div>
-        <div> bad {props.bad} </div>
-        <div> all {props.all} </div>
-        <div> average {props.average} </div>
-        <div> positive {props.positive} </div>
-      </div>
-    )
-    }
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
+  ]
+
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <h1> give feedback </h1>
-      <button onClick={() => setGood(good + 1)}>
-      good
+      {anecdotes[selected]}
+      <button onClick={() => setSelected(getRandomInt(6))}>
+      next anecdote
       </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-      neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-      bad
-      </button>
-      <Statistics
-      clicks= {good + neutral + bad}
-      feedback= {good + neutral + bad}
-      good= {good}
-      neutral= {neutral}
-      bad= {bad}
-      all= {good + neutral + bad}
-      average = {(good - bad) / (good + neutral + bad)}
-      positive = {good / (good + neutral + bad)}/>
     </div>
   )
 }
