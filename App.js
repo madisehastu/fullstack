613472222b5c80d83ed41ce8ba313af2c6b65f26
madisehastu <66207049+madisehastu@users.noreply.github.com>
2022-01-17 16:04:54 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,55 +1,13 @@
-import React, { useState } from 'react'
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const Statistics = (props) => {
-    if (props.clicks === 0) {
-      return (
-        <div>
-          <h1> statistics </h1>
-          <div> No feedback given </div>
-        </div>
-      )
-    }
-    return (
-      <div>
-        <h1> statistics </h1>
-        <div> good {props.good} </div>
-        <div> neutral {props.neutral} </div>
-        <div> bad {props.bad} </div>
-        <div> all {props.all} </div>
-        <div> average {props.average} </div>
-        <div> positive {props.positive} </div>
-      </div>
-    )
-    }
-
-  return (
-    <div>
-      <h1> give feedback </h1>
-      <button onClick={() => setGood(good + 1)}>
-      good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-      neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-      bad
-      </button>
-      <Statistics
-      clicks= {good + neutral + bad}
-      feedback= {good + neutral + bad}
-      good= {good}
-      neutral= {neutral}
-      bad= {bad}
-      all= {good + neutral + bad}
-      average = {(good - bad) / (good + neutral + bad)}
-      positive = {good / (good + neutral + bad)}/>
-    </div>
-  )
-}
-
-export default App
+title New note sequence
+actor user
+user->browser: writes "post new note"
+browser->server: HTTP POST https://studies.cs.helsinki.fi/exampleapp/new_note
+server-->browser: HTML-code
+browser->server: HTTP GET https://studies.cs.helsinki.fi/exampleapp/notes
+server-->browser: HTML-code
+browser->server: HTTP GET https://fullstack-exampleapp.herokuapp.com/main.css
+server-->browser: main.css
+browser->server: HTTP GET https://fullstack-exampleapp.herokuapp.com/main.js
+server-->browser: main.js
+browser->server: https://studies.cs.helsinki.fi/favicon.ico
+server-->browser: text/html
