706b01efe5674fb6a42a030c4fd9d05563616f9a
madisehastu <66207049+madisehastu@users.noreply.github.com>
2022-01-17 16:20:50 +0100

    Update App.js

--- App.js
+++ App.js
@@ -9,5 +9,5 @@ browser->server: HTTP GET https://fullstack-exampleapp.herokuapp.com/main.css
 server-->browser: main.css
 browser->server: HTTP GET https://fullstack-exampleapp.herokuapp.com/main.js
 server-->browser: main.js
-browser->server: https://studies.cs.helsinki.fi/favicon.ico
+browser->server: HTTP GET https://studies.cs.helsinki.fi/favicon.ico
 server-->browser: text/html
