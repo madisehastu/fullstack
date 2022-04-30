77a730ef03433bea3b96d397c3c03733412a3dd4
madisehastu <66207049+madisehastu@users.noreply.github.com>
2022-04-30 19:32:26 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,53 +1,90 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const Statistics = (props) => {
-    if (props.clicks === 0) {
-      return (
-        <div>
-          <h1> statistics </h1>
-          <div> No feedback given </div>
-        </div>
-      )
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('add a new name...')
+  const [newNumber, setNewNumber] = useState('add a new number...')
+  const [searchName, setSearchName] = useState('search phonebook')
+  const [showAll, setShowAll] = useState(true)
+
+  const names = persons.map(person =>
+    person.name + "\t" + person.number)
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      number: newNumber,
     }
-    return (
-      <div>
-        <h1> statistics </h1>
-        <div> good {props.good} </div>
-        <div> neutral {props.neutral} </div>
-        <div> bad {props.bad} </div>
-        <div> all {props.all} </div>
-        <div> average {props.average} </div>
-        <div> positive {props.positive} </div>
-      </div>
-    )
+    setPersons(persons.concat(nameObject))
+    setNewName('add another name...')
+    setNewNumber('add another number')
+  }
+
+  const addOnlyNew = names.includes(newName)
+    ? window.alert(`${newName} is already added to phonebook`)
+    : addName
+
+  const filter = names.filter((person =>
+      person.indexOf(searchName) !== -1))
+
+  const phonebook = persons.map((person) =>
+        <p>{person.name} {person.number}</p>)
+
+  const handleSearchChange = (event) => {
+      console.log(event.target.value)
+      setSearchName(event.target.value)
     }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
 
   return (
     <div>
-      <h1> give feedback </h1>
-      <button onClick={() => setGood(good + 1)}>
-      good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-      neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-      bad
-      </button>
-      <Statistics
-      clicks= {good + neutral + bad}
-      feedback= {good + neutral + bad}
-      good= {good}
-      neutral= {neutral}
-      bad= {bad}
-      all= {good + neutral + bad}
-      average = {(good - bad) / (good + neutral + bad)}
-      positive = {good / (good + neutral + bad)}/>
+      <h1>Phonebook</h1>
+      <form>
+        <div>
+        search: <input
+        value={searchName}
+        onChange={handleSearchChange}/>
+        </div>
+        <div>
+        {filter}
+        </div>
+      </form>
+      <h2> add a new </h2>
+      <form>
+        <div>
+          name: <input
+          value={newName}
+          onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input
+          value={newNumber}
+          onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button onClick={addOnlyNew} type="submit">add</button>
+          <div>debug: {newName} {newNumber}</div>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {phonebook}
+      </div>
     </div>
   )
 }
